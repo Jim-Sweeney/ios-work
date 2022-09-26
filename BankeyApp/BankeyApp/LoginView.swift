bebe2d1613642d6f40ac63a124b723f45c86da99
Jim Sweeney <sweeneyjames.j2@gmail.com>
2022-09-26 19:56:16 -0400

    LoginView started.

---
+++ BankeyApp/BankeyApp/LoginView.swift
@@ -0,0 +1,42 @@
+//
+//  LoginView.swift
+//  BankeyApp
+//
+//  Created by Jim Sweeney on 9/26/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemened")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension LoginView {
+    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func layout() {
+        
+    }
+    
+}
+
+
+
---
+++ BankeyApp/BankeyApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankeyApp
+//
+//  Created by Jim Sweeney on 9/26/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
