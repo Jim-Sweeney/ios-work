c85c4919eeae028bdcb89cd025b206f7488da22e
Jim Sweeney <sweeneyjames.j2@gmail.com>
2022-09-27 19:19:51 -0400

    "Username" Text field added.

--- BankeyApp/BankeyApp/LoginView.swift
+++ BankeyApp/BankeyApp/LoginView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginView: UIView {
     
+    let usernameTextField = UITextField()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -31,13 +33,41 @@ extension LoginView {
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self //protocol-Delegate pattern.
     }
     
     func layout() {
+        addSubview(usernameTextField)
         
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            usernameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: usernameTextField.trailingAnchor, multiplier: 1)
+        ])
     }
     
 }
 
-
+extension LoginView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    // This is a callback to check the text field
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+    
+}
 
