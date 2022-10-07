a1d50a0f95000e2d5a18d4ecacf48b9a9ca576ce
Jim Sweeney <sweeneyjames.j2@gmail.com>
2022-10-06 21:12:57 -0400

    Title added. Log in Successful

--- BankeyApp/BankeyApp/LoginViewController.swift
+++ BankeyApp/BankeyApp/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class LoginViewController: UIViewController {
     
     let loginView = LoginView()
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
     let signInButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
     
@@ -32,6 +34,19 @@ extension LoginViewController {
     private func style() {
         loginView.translatesAutoresizingMaskIntoConstraints = false
         
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.text = "Bankey"
+
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        subtitleLabel.adjustsFontForContentSizeCategory = true
+        subtitleLabel.numberOfLines = 0
+        subtitleLabel.text = "Banking Done Right! "
+        
         signInButton.translatesAutoresizingMaskIntoConstraints = false
         signInButton.configuration = .filled()
         signInButton.configuration?.imagePadding = 8
@@ -47,6 +62,8 @@ extension LoginViewController {
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
         view.addSubview(signInButton)
         view.addSubview(errorMessageLabel)
         
@@ -55,6 +72,19 @@ extension LoginViewController {
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+        
+        // Title
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 3),
+                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+            
+        // Subtitle
+        NSLayoutConstraint.activate([
+            loginView.topAnchor.constraint(equalToSystemSpacingBelow: subtitleLabel.bottomAnchor, multiplier: 3),
+            subtitleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            subtitleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
         ])
         
         //Button
@@ -92,6 +122,11 @@ extension LoginViewController {
             return
         }
         
+        if username == "Patrice Bergeron" && password == "Welcome" {
+            signInButton.configuration?.showsActivityIndicator = true
+        } else {
+            configureView(withMessage: "Incorrect Username or password")
+        }
     }
     
     private func configureView(withMessage message: String) {
